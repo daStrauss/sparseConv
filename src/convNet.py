d7b8b39c7b65302664ce2334f3fb916c4cb78658
Strauss (Air) <dstrauss85@gmail.com>
2012-12-26 08:57:37 -0800

    work on master script.

---
+++ src/convNet.py
@@ -0,0 +1,26 @@
+'''
+Created on Dec 26, 2012
+
+@author: dstrauss
+
+creating some routines to make a convolutional net. especially one that works in parallel
+
+'''
+
+
+def main():
+    ''' main routine '''
+    ''' initialize MPI routine '''
+    
+    
+    ''' load data, given rank '''
+    
+    ''' initialize weights '''
+    
+    ''' begin loop '''
+    
+    ''' for n iterations '''
+    
+    ''' calculate local weights '''
+    
+    ''' all reduce '''
