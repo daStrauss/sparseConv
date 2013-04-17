e337e354e111b48bc38ba6bf86c2080138c409a3
Strauss (Air) <dstrauss85@gmail.com>
2013-04-17 11:40:20 -0700

    update license.

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Methods for content separation using sparse models
 Python extension of the work in the sparseSeparation project
 
 Python development for sparse convolutional models
+
+Python development in parallel with Matlab Development
+
+Licensed under Apache 2.0 license.
--- src/convNetPrime.py
+++ src/convNetPrime.py
@@ -2,6 +2,20 @@
 Created on Dec 26, 2012
 
 @author: dstrauss
+Copyright 2013 David Strauss
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 
 creating some routines to make a convolutional net. especially one that works in parallel
 
